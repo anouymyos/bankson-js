5dd6d9ccf93b7707935bad07eafdeb79dd2daa24
Niklas Närhinen <niklas@narhinen.net>
2016-06-05 03:02:56 +0300

    Add batch download method

--- src/ext/inbound-payments.js
+++ src/ext/inbound-payments.js
@@ -12,4 +12,17 @@ export default class InboundPayments {
   refresh(certificateId) {
     return this.base.post('/inboundpayments', {certificate_id: certificateId});
   }
+
+  batch(batchId, type = 'json') {
+    let opts;
+    if (type !== 'json') {
+      opts = {
+        headers: {
+          'Accept': type === 'xml' ? 'application/xml' : 'text/plain'
+        },
+        responseType: 'arraybuffer'
+      };
+    }
+    return this.base.get(`/inboundpayments/batches/${batchId}`, opts);
+  }
 }
